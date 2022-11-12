c94309082e0b6bfd46496440ed59e5140fe3b328
MILLAN Romain <42139000+Wabez3ter@users.noreply.github.com>
2022-11-12 14:20:32 +0100

    Add Foskcru to project and can have multiple modpack

--- src/main/java/fr/skytorstd/dwm/Main.java
+++ src/main/java/fr/skytorstd/dwm/Main.java
@@ -3,9 +3,7 @@ package fr.skytorstd.dwm;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 import fr.skytorstd.dwm.manager.downloader;
 import org.json.simple.JSONArray;
@@ -16,71 +14,99 @@ public class Main {
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
-        System.out.println(" _____     ______     __     __     __   __     __         ______     ______     _____     __    __     ______     _____     ______    \n" +
-                "/\\  __-.  /\\  __ \\   /\\ \\  _ \\ \\   /\\ \"-.\\ \\   /\\ \\       /\\  __ \\   /\\  __ \\   /\\  __-.  /\\ \"-./  \\   /\\  __ \\   /\\  __-.  /\\  ___\\   \n" +
-                "\\ \\ \\/\\ \\ \\ \\ \\/\\ \\  \\ \\ \\/ \".\\ \\  \\ \\ \\-.  \\  \\ \\ \\____  \\ \\ \\/\\ \\  \\ \\  __ \\  \\ \\ \\/\\ \\ \\ \\ \\-./\\ \\  \\ \\ \\/\\ \\  \\ \\ \\/\\ \\ \\ \\___  \\  \n" +
-                " \\ \\____-  \\ \\_____\\  \\ \\__/\".~\\_\\  \\ \\_\\\\\"\\_\\  \\ \\_____\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\____-  \\ \\_\\ \\ \\_\\  \\ \\_____\\  \\ \\____-  \\/\\_____\\ \n" +
-                "  \\/____/   \\/_____/   \\/_/   \\/_/   \\/_/ \\/_/   \\/_____/   \\/_____/   \\/_/\\/_/   \\/____/   \\/_/  \\/_/   \\/_____/   \\/____/   \\/_____/ ");
-        String modpack = "USB-C";
-        System.out.println("Bienvenue dans le telecharger du modpack " + modpack + "\n\n");
+        System.out.println("______                    _                 _          ___  ___          _     \n" +
+                "|  _  \\                  | |               | |         |  \\/  |         | |    \n" +
+                "| | | |_____      ___ __ | | ___   __ _  __| | ___ _ __| .  . | ___   __| |___ \n" +
+                "| | | / _ \\ \\ /\\ / / '_ \\| |/ _ \\ / _` |/ _` |/ _ \\ '__| |\\/| |/ _ \\ / _` / __|\n" +
+                "| |/ / (_) \\ V  V /| | | | | (_) | (_| | (_| |  __/ |  | |  | | (_) | (_| \\__ \\\n" +
+                "|___/ \\___/ \\_/\\_/ |_| |_|_|\\___/ \\__,_|\\__,_|\\___|_|  \\_|  |_/\\___/ \\__,_|___/" );
+        System.out.println("Bienvenue dans le telecharger de modpack !");
+        System.out.println("Fait par Wabezeter & Foskcru" + "\n\n\n");
 
 
         //CONFIG
         boolean configFolder = false;
-        File configJSON = new File("config.json");
+        File ConfigJsonFile = new File("config.json");
         String folder = "";
-        while(!configJSON.exists()){
+        while(!ConfigJsonFile.exists()){
             (new downloader("https://romainmillan.fr/DWM/config.json", new File("config.json"))).run();
         }
-        if(configJSON.exists()){
-            JSONParser parserConfig = new JSONParser();
-            Object configObj = parserConfig.parse(new FileReader(configJSON));
-            JSONArray jsonArrayConfig = (JSONArray)configObj;
-            JSONObject jsonObjectConfig = (JSONObject) jsonArrayConfig.get(0);
-            folder = (String)jsonObjectConfig.get("folder");
+        if(ConfigJsonFile.exists()){
+            JSONParser ConfigParser = new JSONParser();
+            Object ConfigObject = ConfigParser.parse(new FileReader(ConfigJsonFile));
+            JSONArray ConfigJsonArray = (JSONArray)ConfigObject;
+            JSONObject ConfigJsonObject = (JSONObject) ConfigJsonArray.get(0);
+            folder = (String)ConfigJsonObject.get("folder");
 
             if(!folder.equalsIgnoreCase("")){
-                System.out.print("Un dossier a ete trouve dans la configuration, voulez-vous l'utiliser pour telecharger les mods ? ("+folder+"): [Y/N] ");
-                Scanner scannerConfig = new Scanner(System. in);
-                String resConfig = scannerConfig.nextLine();
-                if (resConfig.equalsIgnoreCase("Y")){
+                System.out.print("Un dossier a ete trouve dans la configuration, voulez-vous l'utiliser pour telecharger les mods ? ('"+folder+"'): [Y/N] ");
+                Scanner ConfigScanner = new Scanner(System. in);
+                String ConfigResultat = ConfigScanner.nextLine();
+                if (ConfigResultat.equalsIgnoreCase("Y")){
                     configFolder = true;
                 }
             }
         }
         if(configFolder == false){
             System.out.print("Entrer le dossier ou telecharger les mods: ");
-            Scanner scanner = new Scanner(System. in);
-            folder = scanner.nextLine();
-            System.out.println("Dossier enregistre ("+folder+")");
+            Scanner FolderModsScanner = new Scanner(System. in);
+            folder = FolderModsScanner.nextLine();
+            System.out.println("Dossier enregistre ('"+folder+"')");
         }
 
 
         //LINK JSON
-        JSONParser parserJSONFile = new JSONParser();
-        Object jsonFileObj = parserJSONFile.parse(new FileReader(configJSON));
-        JSONArray jsonArrayjsonFile = (JSONArray)jsonFileObj;
-        JSONObject jsonObjectjsonFile = (JSONObject) jsonArrayjsonFile.get(0);
+        JSONParser ConfigModpacksParser = new JSONParser();
+        Object ConfigModpackObject = ConfigModpacksParser.parse(new FileReader(ConfigJsonFile));
+        JSONArray ConfigModPackJsonArray = (JSONArray)ConfigModpackObject;
+        JSONObject ConfigModpackJsonObject = (JSONObject) ConfigModPackJsonArray.get(0);
 
-        String urlJSON = (String) jsonObjectjsonFile.get("linkJSON");
-        //(new downloader(urlJSON, new File("mods.json"))).run();
+        Map ConfigModpackAddress = ((Map)ConfigModpackJsonObject.get("modpacks"));
+        Iterator<Map.Entry> itrConfigFile = ConfigModpackAddress.entrySet().iterator();
+
+        HashMap<String, String> allNameAndLinkModPack = new HashMap<>();
+        while(itrConfigFile.hasNext()){
+            Map.Entry ConfigModpackPair = itrConfigFile.next();
+            allNameAndLinkModPack.put((String)ConfigModpackPair.getKey(), (String)ConfigModpackPair.getValue());
+        }
+
+
+        String modpackSelect = "";
+        do{
+            System.out.print("\nSelectioner le Modpack (");
+            Iterator<Map.Entry<String, String>> AllNameAndLinkModpackIterator = allNameAndLinkModPack.entrySet().iterator();
+            while(AllNameAndLinkModpackIterator.hasNext()){
+                Map.Entry set = AllNameAndLinkModpackIterator.next();
+                System.out.print(set.getKey());
+                if(AllNameAndLinkModpackIterator.hasNext()){
+                    System.out.print("/");
+                }
+            }
+            System.out.print("): ");
+            Scanner ModpackSelectorScanner = new Scanner(System. in);
+            modpackSelect = ModpackSelectorScanner.nextLine();
+        }while (!allNameAndLinkModPack.containsKey(modpackSelect));
+        String ModpackJsonURL = allNameAndLinkModPack.get(modpackSelect);
+
+        String ModpackNameJSON = modpackSelect+".json";
+        (new downloader(ModpackJsonURL, new File(ModpackNameJSON))).run();
 
 
         //DOWNLOAD ALL MODS
         try {
-            JSONParser parser = new JSONParser();
-            parser = new JSONParser();
-            Object obj = parser.parse(new FileReader("mods.json"));
-            JSONArray jsonarray = (JSONArray)obj;
+            JSONParser ModsParser = new JSONParser();
+            ModsParser = new JSONParser();
+            Object ModsObject = ModsParser.parse(new FileReader(ModpackNameJSON));
+            JSONArray ModsJsonArray = (JSONArray)ModsObject;
 
-            JSONObject jsonObject = (JSONObject)jsonarray.get(0);
+            JSONObject ModsJsonObject = (JSONObject)ModsJsonArray.get(0);
 
-            Map address = ((Map)jsonObject.get("mods"));
+            Map ModsAddress = ((Map)ModsJsonObject.get("mods"));
 
-            Iterator<Map.Entry> itr1 = address.entrySet().iterator();
-            int currentMod = 1;
-            while (itr1.hasNext()) {
-                Map.Entry pair = itr1.next();
+            Iterator<Map.Entry> ModsIterator = ModsAddress.entrySet().iterator();
+            int currentModInt = 1;
+            while (ModsIterator.hasNext()) {
+                Map.Entry pair = ModsIterator.next();
                 String nameMod = (String) pair.getKey();
                 String linkModFile = "";
                 if(folder.endsWith("\\")){
@@ -90,17 +116,15 @@ public class Main {
                 }
                 File modFile = new File(linkModFile);
                 if (!modFile.exists()){
-                    (new downloader((String) pair.getValue(), modFile)).downloadmod(address.size(), currentMod);
-                    //System.out.println(nameMod + " telecharge");
+                    (new downloader((String) pair.getValue(), modFile)).downloadmod(ModsAddress.size(), currentModInt);
                 }else {
                     System.out.println(nameMod + " est deja present dans le dossier");
                 }
-                currentMod++;
+                currentModInt++;
             }
 
 
             System.out.println("\n\nTous les mods on etait telecharge avec succes !");
-            Thread.sleep(100);
             System.exit(1);
         }catch(Exception ex) {
             ex.printStackTrace();
